b9e152585678ceb0583c966d457e83b5d01bd1da
Bill Chen <Bill.Chen@live.com>
2020-12-18 23:50:26 +0800

    Update diagrams.

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ * Main class
+ *
+ * @author billchen
+ * @version 1.0
+ * @create 2020-12-18 17:03
+ **/
+public class main {
+
+	public static void main(String[] args) {
+
+	}
+}
